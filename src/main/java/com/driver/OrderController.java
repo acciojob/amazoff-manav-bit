f8bd964a4bec6952e57647ef0db4a9ad96651fb6
MANAV AHUJA <ahujamanav43@gmail.com>
2024-03-18 22:35:55 +0530

    done4

--- src/main/java/com/driver/OrderController.java
+++ src/main/java/com/driver/OrderController.java
@@ -20,7 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController {
 
     @Autowired
-    private OrderService orderService;
+    OrderService orderService=new OrderService() ;
 
     @PostMapping("/add-order")
     public ResponseEntity<String> addOrder(@RequestBody Order order){
